259e26ddb1560dbe921d4b4885fea85332c67026
Beyzabay <beyza.bay@icloud.com>
2024-05-24 13:28:36 +0300

    previewing, adding, deleting and updating photos has been completed.

--- src/app/Components/ProfilePhotoUpload .jsx
+++ src/app/Components/ProfilePhotoUpload .jsx
@@ -1,19 +1,120 @@
-import React from 'react'
-import Image from 'next/image';
+"use client"
+import React, { useState } from 'react';
 
-const ProfilePhotoUpload  = () => {
+const ProfilePhotoUpload = () => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [image, setImage] = useState('');
+  const [selectedImage, setSelectedImage] = useState('');
+
+  const openPopup = () => {
+    setIsPopupOpen(true);
+    setImage(selectedImage);
+  };
+
+  const closePopup = () => {
+    setIsPopupOpen(false);
+  };
+
+  // Kullanıcının dosya seçtiğinde çalışan fonksiyon
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader(); 
+
+    reader.onloadend = () => {
+      setImage(reader.result);
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
+  // Görüntüyü güncelleme işlemini yöneten fonksiyon
+  const handleImageUpdate = (event) => {
+    event.preventDefault();
+    setSelectedImage(image); 
+    setIsPopupOpen(false); 
+  };
+
+  // Görüntüyü silme işlemini yöneten fonksiyon
+  const handleImageDelete = () => {
+    setSelectedImage(''); 
+    setImage(''); 
+    setIsPopupOpen(false); 
+  };
+
   return (
-<div className="container flex items-center justify-center">
-      <div className="w-48 h-48 overflow-hidden rounded-full">
-        <img 
-          src="https://media.istockphoto.com/id/1386479313/tr/foto%C4%9Fraf/happy-millennial-afro-american-business-woman-posing-isolated-on-white.jpg?s=612x612&w=0&k=20&c=X0duG4XnaH_BMWk9S2k0h96ztBAB7bguOL0e6iYljIY=" 
-          alt="Happy Millennial Afro American Business Woman" 
+    <div className="container flex items-center justify-center">
+      <div className="w-48 h-48 overflow-hidden rounded-full" onClick={openPopup}>
+        <img
+          src={selectedImage ? selectedImage : "https://media.istockphoto.com/id/1386479313/tr/foto%C4%9Fraf/happy-millennial-afro-american-business-woman-posing-isolated-on-white.jpg?s=612x612&w=0&k=20&c=X0duG4XnaH_BMWk9S2k0h96ztBAB7bguOL0e6iYljIY="}
+          alt="Happy Millennial Afro American Business Woman"
           className="object-cover w-full h-full rounded-full"
         />
       </div>
+      {/* Popup penceresi */}
+      {isPopupOpen && (
+        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-white p-8 rounded-lg relative">
+            <div className='flex justify-around'>
+              <img
+                src={image ? image : "https://freesvg.org/img/abstract-user-flat-4.png"}
+                alt="Selected Image"
+                className="object-cover h-40 w-40 rounded-lg mb-4"
+              />
+            </div>
+            {/* Dosya yükleme inputu */}
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
+              className="hidden"
+              id="image-upload"
+            />
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
+              className="border rounded-md mt-2 p-1 w-full"
+            />
+            {/* Butonlar */}
+            <div className="flex items-start mt-4 gap-5">
+              {image && (
+                <button className="bg-green-500 text-white px-4 py-2 rounded-md" onClick={handleImageUpdate}>
+                  Güncelle
+                </button>
+              )}
+              {!selectedImage && (
+                <button className="bg-blue-500 text-white px-4 py-2 rounded-md" onClick={handleImageUpdate}>
+                 Fotoğraf ekle
+                </button>
+              )}
+              {selectedImage && (
+                <button className="bg-red-500 text-white px-4 py-2 rounded-md" onClick={handleImageDelete}>
+                  Delete
+                </button>
+              )}
+            </div>
+            <button className="absolute top-0 right-0 m-4" onClick={closePopup}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 text-gray-600"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      )}
     </div>
-
-  )
-}
-
-export default ProfilePhotoUpload 
+  );
+};
+export default ProfilePhotoUpload;
